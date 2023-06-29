fb53fb5f5bf76885775aa290a92f943fb57a121b
Ashwin V C <vcashwin99@gmail.com>
2023-06-29 18:57:46 +0530

    Add Footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/sections/footer";
 import { Memberships } from "@/sections/memberships";
 import { PastCompanies } from "@/sections/past-companies";
 import { Testimonials } from "@/sections/testimonials";
@@ -17,7 +18,7 @@ export default function Home() {
       </Head>
       <div className="relative flex min-h-full flex-col items-center overflow-hidden font-heading gap-16">
         <div className="absolute inset-0 bg-[url(../assets/images/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-        <div className="pt-8 text-lg">AssembleGrowth</div>
+        <div className="pt-8 text-5xl">AssembleGrowth</div>
         <h1 className="flex gap-2 justify-center text-xl font-bold lg:tracking-wide xl:text-2xl max-w-2xl text-center">
           <div data-text="Coming">
             <span className="animate-gradient-1 bg-gradient-to-r from-red-500 to-orange-500  bg-clip-text text-transparent">
@@ -32,14 +33,14 @@ export default function Home() {
           </div>
         </h1>
         <div className="sm:max-w-auto relative flex flex-col gap-10 px-10 py-20 sm:mx-auto sm:rounded-lg sm:px-20 bg-lightorange">
-          <h1 className="text-5xl font-bold lg:tracking-tight xl:text-7xl max-w-2xl text-center">
+          <h1 className="text-2xl font-bold lg:tracking-tight xl:text-2xl max-w-2xl text-center">
             The Next-Gen web development agency
           </h1>
           <h1 className="font-sans text-xl lg:tracking-wider xl:text-xl max-w-2xl text-center">
             A single subscription to 10x your business.
           </h1>
         </div>
-        {/* <section className="flex justify-center w-full">
+        <section className="flex justify-center w-full">
           <PastCompanies />
         </section>
         <section className="flex justify-center w-full">
@@ -47,7 +48,10 @@ export default function Home() {
         </section>
         <section className="flex justify-center w-full">
           <Memberships />
-        </section> */}
+        </section>
+        <section className="flex justify-center w-full">
+          <Footer />
+        </section>
       </div>
     </>
   );
